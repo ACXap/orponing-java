78013180e8ebd1ff5051694b42d64eb135fe3af4
Aleksandr <mitkov1@inbox.ru>
2021-02-13 15:04:01 +0700

    optim

--- src/test/java/com/rt/orponing/controllers/AddressOrponingControllerTest.java
+++ src/test/java/com/rt/orponing/controllers/AddressOrponingControllerTest.java
@@ -13,20 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.Mockito.when;
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.hamcrest.Matchers.containsString;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AddressOrponingController.class)
@@ -47,11 +42,14 @@ class AddressOrponingControllerTest {
         Mockito.when(service.orponingAddress(ArgumentMatchers.any(EntityAddress.class))).thenReturn(adr);
 
         MvcResult mvcResult = mockMvc.perform(get("/api/get_global_id").param("address", "Новосибирск г., Орджоникидзе ул., дом 18"))
+
                 .andExpect(request().asyncStarted())
+
                 .andDo(MockMvcResultHandlers.log())
                 .andReturn();
 
         mockMvc.perform(asyncDispatch(mvcResult))
+
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith("application/json"))
                 .andExpect(content().json("{'GlobalId':29182486}"))
