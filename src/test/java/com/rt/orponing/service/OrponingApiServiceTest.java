86c2048779803d7082f88571f2c4bf5392560adf
Aleksandr <mitkov1@inbox.ru>
2021-02-14 13:49:18 +0700

    optim

--- src/test/java/com/rt/orponing/service/OrponingApiServiceTest.java
+++ src/test/java/com/rt/orponing/service/OrponingApiServiceTest.java
@@ -1,11 +1,19 @@
 package com.rt.orponing.service;
 
 import com.rt.orponing.repository.data.AddressInfo;
+import com.rt.orponing.service.config.ServiceTestConfig;
+import com.rt.orponing.service.config.ServiceTestStopConfig;
 import com.rt.orponing.service.data.Status;
 import com.rt.orponing.service.data.StatusType;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,46 +21,53 @@ import java.util.UUID;
 import java.util.concurrent.RejectedExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ServiceTestConfig.class, ServiceTestStopConfig.class})
+@ActiveProfiles("test")
 class OrponingApiServiceTest {
 
     @Autowired
-    private OrponingApiService _service;
+    private OrponingApiService service;
+
+    @MockBean
+    private OrponingService os;
 
     @Test
     void addTask() {
-        String taskId =_service.addTask(new ArrayList<>());
+        String taskId = service.addTask(new ArrayList<>());
 
         assertNotEquals(null, taskId);
     }
 
     @Test
-    void getResultTask_goodId()throws Exception {
-        String taskId =_service.addTask(new ArrayList<>());
+    void getResultTask_goodId() throws Exception {
+        String taskId = service.addTask(new ArrayList<>());
         List<AddressInfo> list = null;
 
-        try{
-            list = _service.getResultTask(taskId);
+        try {
+            list = service.getResultTask(taskId);
             fail();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             assertEquals("Task not completed", ex.getMessage());
         }
 
-        if(list==null){
+        if (list == null) {
             Thread.sleep(2000);
         }
-        list = _service.getResultTask(taskId);
+        list = service.getResultTask(taskId);
 
+        Mockito.verify(os, times(1)).setAddressById(ArgumentMatchers.anyList());
         assertNotEquals(null, list);
     }
 
     @Test
     void getResultTask_errorId() throws Exception {
-        try{
-            List<AddressInfo> list = _service.getResultTask("12312313");
+        try {
+            List<AddressInfo> list = service.getResultTask("12312313");
             fail();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             assertEquals("Id not correct", ex.getMessage());
         }
     }
@@ -61,41 +76,42 @@ class OrponingApiServiceTest {
     void getResultTask_notFoundId() throws Exception {
         UUID uuid = UUID.randomUUID();
 
-        try{
-            List<AddressInfo> list = _service.getResultTask(uuid.toString());
+        try {
+            List<AddressInfo> list = service.getResultTask(uuid.toString());
             fail();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             assertEquals("Id not found", ex.getMessage());
         }
     }
 
     @Test
     void getStatusTask() throws Exception {
-        String taskId =_service.addTask(new ArrayList<>());
-        Status status = _service.getStatusTask(taskId);
+        String taskId = service.addTask(new ArrayList<>());
+        Status status = service.getStatusTask(taskId);
 
         assertEquals(StatusType.STOP, status.getStatus());
     }
 
     @Test
     void getStatusTask_waitResult() throws Exception {
-        String taskId =_service.addTask(new ArrayList<>());
+        String taskId = service.addTask(new ArrayList<>());
 
         Thread.sleep(2000);
-        Status status = _service.getStatusTask(taskId);
+        Status status = service.getStatusTask(taskId);
+
+        Mockito.verify(os, times(1)).setAddressById(ArgumentMatchers.anyList());
         assertEquals(StatusType.COMPLETED, status.getStatus());
     }
 
     @Test
-    void testManyRequest()  {
+    void testManyRequest() {
 
-        try{
-            for(int i = 0; i<25;i++){
-                _service.addTask(new ArrayList<>());
+        try {
+            for (int i = 0; i < 65; i++) {
+                service.addTask(new ArrayList<>());
             }
-
             fail();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             assertTrue(ex instanceof RejectedExecutionException);
         }
     }
@@ -103,11 +119,11 @@ class OrponingApiServiceTest {
     @Test
     void testManyRequest_enoughThread() {
 
-        try{
-            for(int i = 0; i<14;i++){
-                _service.addTask(new ArrayList<>());
+        try {
+            for (int i = 0; i < 14; i++) {
+                service.addTask(new ArrayList<>());
             }
-        } catch (Exception ex){
+        } catch (Exception ex) {
             fail();
         }
     }
--- src/test/java/com/rt/orponing/service/config/ServiceTestConfig.java
+++ src/test/java/com/rt/orponing/service/config/ServiceTestConfig.java
@@ -2,6 +2,7 @@ package com.rt.orponing.service.config;
 
 import com.rt.orponing.dao.interfaces.IDbAddress;
 import com.rt.orponing.repository.IRepositoryOrpon;
+import com.rt.orponing.service.OrponingApiService;
 import com.rt.orponing.service.OrponingService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +13,10 @@ public class ServiceTestConfig {
     @Bean
     public OrponingService orponingService(IRepositoryOrpon repository, IDbAddress db ){
         return new OrponingService(repository, db);
+    }
+
+    @Bean
+    public OrponingApiService orponingApiService(OrponingService service ){
+        return new OrponingApiService(service);
     }
 }
